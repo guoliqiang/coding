3ebecb253c36f0f884e1bf371967a03d7a25eee4
LiqiangGuo <guoliqiang2006@gmail.com>
2013-04-02 22:50:34 +0800

    has updated

--- test.cc
+++
@@ -1,7 +0,0 @@
-// Copyright 2013 Jike Inc. All Rights Reserved.
-// Author: Liqiang Guo(guoliqiang@jike.com)
-// I just want to GH to hss~
-// Date  : 2013-04-02 22:45:08
-// File  : test.cc
-// Brief :
-
