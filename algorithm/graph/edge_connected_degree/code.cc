fe386fcf4750b9aef6144cd77c0b30cd72d41777
LiqiangGuo <guoliqiang2006@gmail.com>
2013-10-15 22:25:30 +0800

    has updated

--- algorithm/graph/edge_connected_degree/code.cc
+++ algorithm/graph/edge_connected_degree/code.cc
@@ -3,9 +3,18 @@
 // I just want to GH to hss~
 // Date  : 2013-10-14 21:30:43
 // File  : code.cc
-// Brief :
+// Brief :  TODO(guoliqiang) debug poj1966 一直ＷＡ
 // 最少去掉多少条边，能是图变得不连通
 // 边连通度  ==  最小割  == 最大流
+
+// 若G为无向图：
+// 1.     原G图中的每条边e=UV变成两条边e`=UV,e``=VU,容量都为1；
+// 2.     固定一个点为源点，枚举与汇点，求最大流max_flow，保留最小的max_flow即为图的边连通度。
+// 若G为有向图：
+// 1.     原G图中每条有向边容量为1
+// 2.     固定一个点为源点，枚举与汇点，求最大流max_flow，保留最小的max_flow即为图的边连通度。
+// 这里说的有向图的连通性是强连通
+
 #include "base/public/common_head.h"
 
 namespace algorithm {
@@ -16,8 +25,15 @@ int matrix[MAX][MAX];
 int visited[MAX];
 int pre[MAX];
 int flow[MAX][MAX];
+int cut[MAX][MAX];
 
 int MaxFlow(int source, int target) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (matrix[i][j] > 0) flow[i][j] = 1; // 如果是无向图需要设置flow[j][i] = 1
+    }
+  }
+
   int rs = 0;
   while (true) {
     memset(pre, 0, sizeof(pre));
@@ -51,14 +67,30 @@ int MaxFlow(int source, int target) {
 }
 
 int EdgeConnectedDegree() {
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      if (matrix[i][j] > 0) flow[i][j] = 1;  // 如果是无向图需要设置flow[j][i] = 1
+  int rs = INT_MAX;
+  int target = - 1;
+  for (int i = 1; i < n; i++) {
+    int tmp = MaxFlow(0, i);
+    if (tmp < rs) {
+      rs = tmp;
+      target = i;
     }
   }
-  int rs = INT_MAX;
-  for (int i = 1; i < n; i++) {
-    rs = std::min(rs, MaxFlow(0, i));
+  int tmp = rs;
+  // 循环删除边看是否回影响最大流
+  memset(cut, 0, sizeof(cut));
+  for (int i = 0; i < n && tmp; i++) {
+    for (int j = 0; j < n && tmp; j++) {
+      if (matrix[i][j] > 0) {
+        matrix[i][j] = 0;
+        int cur = MaxFlow(0, target);
+        if (cur < rs) {
+          cut[i][j] = 1;
+          tmp--;
+        }
+        else matrix[i][j] = 1;
+      }
+    }
   }
   return rs;
 }
---
+++ algorithm/graph/point_connect_degree/code.cc
@@ -0,0 +1,183 @@
+// Copyright 2013 Jike Inc. All Rights Reserved.
+// Author: Liqiang Guo(guoliqiang@jike.com)
+// I just want to GH to hss~
+// Date  : 2013-10-15 17:18:59
+// File  : code.cc
+// TODO(guoliqiang) debug poj1966 WA
+// Brief : http://www.cnblogs.com/dyllove98/archive/2013/07/25/3215003.html
+//         http://www.cnblogs.com/-hsz/archive/2012/07/24/2607375.html
+//
+// 点连通度：
+//　　求一个给定的无向图的点连通度，可以转换为求边连通度，怎么转换就如下所示：
+// 
+// 将点连通度转化为边连通度时怎么建图呢：
+// 1 ．构造一个网络 N
+//  若 G 为无向图：
+//   (1) 原 G 图中的每个顶点 v 变成 N 网中的两个顶点 v' 和 v" ，顶点 v' 至 v" 有一条弧（有向边）连接，弧容量为 1;
+//   (2) 原 G 图中的每条边 e ＝ uv ，在 N 网中有两条弧 e’= u"v',e"=v"u' 与之对应， e' 弧容量为 ∞ ， e" 弧容量为 ∞
+//   (3)A” 为源顶点， B' 为汇顶点
+//    注意：弧是有向边
+//  若 G 为有向图：
+//    (1) 原 G 图中的每个顶点变成 N 网中的两个顶点 v’ 和 v” ，顶点 v' 至 v” 有一条弧连接，弧容量为 1
+//    (2) 原 G 图中的每条弧 e ＝ uv 变成一条有向轨 u'u"v'v" ，其中轨上的弧 u"v' 的容量为 ∞;
+//    (3)A” 为源顶点， B' 为汇顶点
+//
+// 2 ．指定一个源点 A" ，枚举汇点B'，求 A" 到 B' 的最大流 F ，
+//   　注意　Ａ'' B' 是说他们来自于不同的两个点
+
+#include "base/public/common_head.h"
+#include <stdio.h>
+namespace algorithm {
+const int MAX = 1000;
+int n;
+int pre[MAX] = {0};
+int visited[MAX] = {0};
+int flow[MAX][MAX];
+int matrix[MAX][MAX];
+int cut[MAX] = {0};
+
+int MaxFlow(int source, int target) {
+  int rs = 0;
+  for (int i = 0; i < n * 2; i++) {
+    for (int j = 0; j < n * 2; j++) {
+      flow[i][j] = matrix[i][j];
+    }
+  }
+  while (true) {
+    memset(pre, 0, sizeof(pre));
+    memset(visited, 0, sizeof(visited));
+    std::queue<int> queue;
+    queue.push(source);
+    visited[source] = 1;
+    while (!queue.empty()) {
+      int t = queue.front();
+      queue.pop();
+      if (visited[target] == 1) break;
+      for (int i = 0; i < 2 * n; i++) {
+        if (visited[i] == 0 && flow[t][i] > 0) {
+          queue.push(i);
+          pre[i] = t;
+          visited[i] = 1;
+        }
+      }
+    }
+    if (visited[target] == 0) break;
+    int min = INT_MAX;
+    for (int i = target; i != source; i = pre[i]) {
+      min = std::min(min, flow[pre[i]][i]);
+    }
+    for (int i = target; i != source; i = pre[i]) {
+      flow[pre[i]][i] -= min;
+      flow[i][pre[i]] += min;
+    }
+    // LOG(INFO) << "min:" << min << " rs :" << rs;
+    // 防止越界
+    if (rs != INT_MAX) {
+      if (min == INT_MAX) rs = min;
+      else rs += min;
+    }
+   
+  }
+  return rs;
+}
+
+int UnDirected(std::vector<std::vector<int> > & v) {
+  memset(matrix, 0, sizeof(matrix));
+  memset(flow, 0, sizeof(flow));
+
+  n = v.size();
+  int k = -1;
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      if (v[i][j] > 0) {
+        matrix[i][n + i] = 1;
+        matrix[j][n + j] = 1;
+        matrix[n + i][j] = INT_MAX;
+        matrix[n + j][i] = INT_MAX;
+        if (k == -1) k = n + i;
+      }
+    }
+  }
+  int rs = INT_MAX;
+  int target = -1;
+  for (int i = 0; i < n; i++) {
+    if (i + n == k) continue;
+    int t = MaxFlow(k, i);
+    if (t < rs) {
+      rs = t;
+      target = i;
+    }
+  }
+  if (rs == INT_MAX) {// 强连通图，需要去掉所有的点
+    return n;
+  } else {
+    memset(cut, 0, sizeof(cut));
+    int flow_value = rs;
+    for (int i = 0; i < n && flow_value; i++) {
+      if (i == target || i == k - n) continue;
+      matrix[i][n + i] = 0;
+      if (MaxFlow(k, target) < flow_value) {
+        cut[i] = 1;
+        flow_value--;
+      } else {
+        matrix[i][n + i] = 1;
+      }
+    }
+    return rs;
+  }
+}
+
+int Directed(std::vector<std::vector<int> > & v) {
+  memset(matrix, 0, sizeof(matrix));
+  memset(flow, 0, sizeof(flow));
+
+  n = v.size();
+  int k = -1;
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; i < n; i++) {
+      if (v[i][j] > 0) {
+        matrix[i][n + i] = 1;
+        matrix[j][n + j] = 1;
+        matrix[n + i][j] = INT_MAX;  // Note here
+        if (k == -1) k = n + i;
+      }
+    }
+  }
+  int rs = INT_MAX;
+  int target = -1;
+  for (int i = 0; i < n; i++) {
+    if (i + n == k) continue;
+    int t = MaxFlow(k, i);
+    if (t < rs) {
+      rs = t;
+      target = i;
+    }
+  }
+  if (rs == INT_MAX) { // 强连通图，需要去掉所有的点
+    return n;
+  } else {
+    memset(cut, 0, sizeof(cut));
+    int flow_value = rs;
+    for (int i = 0; i < n && flow_value; i++) {
+      if (i == target || i == k - n) continue;
+      matrix[i][n + i] = 0;
+      if (MaxFlow(k, target) < flow_value) {
+        cut[i] = 1;
+        flow_value--;
+      } else {
+        matrix[i][n + i] = 1;
+      }
+    }
+    return rs;
+  }
+}
+
+}  // namespace algorithm
+
+using namespace algorithm;
+
+int main(int argc, char** argv) {
+  std::vector<std::vector<int> > v(3, std::vector<int>(3, 1));
+  LOG(INFO) << UnDirected(v);
+  return 0;
+}
