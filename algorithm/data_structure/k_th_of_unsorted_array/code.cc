5ab072c342a3585402530865214c6be9d561b8d9
guoliqiang <guoliqiang2006@126.com>
2013-11-03 17:35:58 -0800

    has updated

---
+++ algorithm/data_structure/k_th_of_unsorted_array/code.cc
@@ -0,0 +1,58 @@
+// Copyright 2013 Jike Inc. All Rights Reserved.
+// Author: Liqiang Guo(guoliqiang@jike.com)
+// I just want to GH to hss~
+// Date  : 2013-11-03 04:13:05
+// File  : code.cc
+// Brief :
+
+#include "base/public/common_head.h"
+
+namespace algorithm {
+const int MAX = 1000;
+int N = 0;
+int data[MAX] = {0};
+
+int Kth(int k, int b, int e) {
+  if (k > e) return -1;
+  int mid = b + (e - b) / 2;
+  std::swap(data[mid], data[e]);
+  int t = -1;
+  for (int i = b; i < e; i++) {
+    if (data[i] < data[e]) std::swap(data[i], data[++t]);
+  }
+  std::swap(data[++t], data[e]);
+  if (t == k) return data[t];
+  else {
+    if (t > k) return Kth(k, b, mid);
+    else {
+      return Kth(k - t - 1, mid + 1, e);
+    }
+  }
+}
+
+int Kth(int k) {
+  return Kth(k - 1, 0, N - 1);
+}
+
+void Read(std::vector<int> & v) {
+  memset(data, 0, sizeof(data));
+  N = v.size();
+  for (int i = 0; i < N; i++) {
+    data[i] = v[i];
+  }
+}
+}  // namespace algorithm
+
+using namespace algorithm;
+
+int main(int argc, char** argv) {
+  std::vector<int> v;
+  v.push_back(10);
+  v.push_back(3);
+  v.push_back(5);
+  v.push_back(1);
+  Read(v);
+  LOG(INFO) << Kth(1);
+  LOG(INFO) << Kth(3);
+  return 0;
+}
---
+++ algorithm/data_structure/reverse_num_to_permutation/code.cc
@@ -0,0 +1,51 @@
+// Copyright 2013 Jike Inc. All Rights Reserved.
+// Author: Liqiang Guo(guoliqiang@jike.com)
+// I just want to GH to hss~
+// Date  : 2013-11-03 06:09:00
+// File  : code.cc
+// Brief :
+
+#include "base/public/common_head.h"
+
+/*
+ * f(n, m) 表示n个数据，逆序数为m时的不同排列的个数。
+ * 则 f(n + 1, m) = f(n, m) + f(n, m - 1) + f(n, m -2) + ... f(n, m - n)  // 根据第n+1个数产生的逆序数分类
+ *    
+ *    f(n + 1, m) = f(n, m) + f(n + 1, m - 1) - f(n, m - n - 1)
+ *
+ * */
+
+namespace algorithm {
+const int MAX = 1000;
+int N = 0;
+int dp[MAX][MAX] = {{0}};
+
+void DP() {
+  memset(dp, 0, sizeof(dp));
+  dp[1][0] = dp[2][0] = dp[2][1] = 1;
+  for (int i = 3; i <= N; i++) {
+    dp[i][0] = 1;
+    int m = i * (i - 1) / 2;
+    for (int j = 1; j <= m; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      if (j - i >= 0) dp[i][j] -= dp[i - 1][j - i];
+    }
+  }
+}
+
+int Get(int i, int j) {
+  return dp[i][j];
+}
+
+}  // namespace algorithm
+
+using namespace algorithm;
+
+
+int main(int argc, char** argv) {
+  N = 5;
+  DP();
+  LOG(INFO) << Get(3, 2);
+  LOG(INFO) << Get(4, 2);
+  return 0;
+}
