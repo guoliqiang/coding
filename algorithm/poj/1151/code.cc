1f72131165c6c6b1da0e395beeeff39d162c09f2
guoliqiang <guoliqiang2006@126.com>
2013-11-06 05:01:39 -0800

    has udpated

---
+++ algorithm/poj/1151/code.cc
@@ -0,0 +1,12 @@
+// Copyright 2013 Jike Inc. All Rights Reserved.
+// Author: Liqiang Guo(guoliqiang@jike.com)
+// I just want to GH to hss~
+// Date  : 2013-11-06 05:00:23
+// File  : code.cc
+// Brief :
+
+/*
+ * 线段树求举行并的面积。
+ * http://wenku.baidu.com/link?url=j05jLbF89y0fyVfw7Vi1RYd5GoWfoc5MKAq8piZCBMhZcLsQo-pqt2GsCvnsGPPRkFjdiPb2Jh6He4DwEHD0A1scSgiMPjxt-AiiBBr0XWm
+ *
+ * */
---
+++ algorithm/poj/1177/code.cc
@@ -0,0 +1,11 @@
+// Copyright 2013 Jike Inc. All Rights Reserved.
+// Author: Liqiang Guo(guoliqiang@jike.com)
+// I just want to GH to hss~
+// Date  : 2013-11-06 04:58:54
+// File  : code.cc
+// Brief :
+
+/*
+ *  线段树求矩形并的周长
+ *  http://www.cnblogs.com/Booble/archive/2010/10/10/1847163.html
+ * */
