44baedf63acbb9a6af6a03674b9ea29993fd7711
guoliqiang <guoliqiang2006@126.com>
2013-11-06 02:44:34 -0800

    add st to solve reverse number

---
+++ algorithm/data_structure/reverse_num/code2.cc
@@ -0,0 +1,108 @@
+// Copyright 2013 Jike Inc. All Rights Reserved.
+// Author: Liqiang Guo(guoliqiang@jike.com)
+// I just want to GH to hss~
+// Date  : 2013-11-06 02:16:21
+// File  : code2.cc
+// Brief :
+
+/*
+ * 使用线段数统计
+ * */
+#include "base/public/common_head.h"
+
+namespace algorithm {
+const int MAX = 1000;
+struct DNode {
+  int val;
+  int idx;
+} data[MAX];
+
+struct Node {
+  int l;
+  int r;
+  int num;
+} node[MAX * 3];
+
+struct SNode {
+  int val;
+  int id;
+} sdata[MAX];
+
+bool Cmp(const SNode & x, const SNode & y) {
+  return x.val < y.val;
+}
+
+void Build(int k, int l, int r) {
+  node[k].l = l;
+  node[k].r = r;
+  node[k].num = 0;
+  if (l == r) return;
+  int mid = l + (r - l) / 2;
+  Build(2 * k, l, mid);
+  Build(2 * k + 1, mid + 1, r);
+}
+
+int Query(int k, int l, int r) {
+  if (node[k].l >= l && node[k].r <= r) return node[k].num;
+  else {
+    int mid = node[k].l + (node[k].r - node[k].l) / 2;
+    if (mid >= r) return Query(2 * k, l, r);
+    else if (mid < l) return Query(2 * k + 1, l, r);
+    else {
+      return Query(2 * k, l, mid) + Query(2 * k + 1, mid + 1, r);
+    }
+  }
+}
+
+void Update(int k, int v) {
+  if (node[k].l <= v && node[k].r >= v) node[k].num++;
+  if (node[k].l == node[k].r) return;
+  int mid = node[k].l + (node[k].r - node[k].l) / 2;
+  if (v <= mid) Update(2 * k, v);
+  else Update(2 * k + 1, v);
+}
+
+int Read(std::vector<int> & v) {
+  // 离散化
+  for (int i = 0; i < v.size(); i++) {
+    data[i].val = v[i];
+    data[i].idx = -1;
+    sdata[i].val = v[i];
+    sdata[i].id = i;
+  }
+  SNode * ptr = sdata;
+  std::sort(ptr, ptr + v.size(), Cmp);
+  int val = - 1;
+  int k = 0;
+  for (int i = 0; i < v.size(); i++) {
+    if (k == 0 || val != ptr[i].val) {
+      k++;
+      val = ptr[i].val;
+    };
+    data[ptr[i].id].idx = k;
+  }
+  Build(1, 1, k);
+  int rs = 0;
+  for (int i = 0; i < v.size(); i++) {
+    int t = data[i].idx;
+    if (t < k) rs += Query(1, t + 1, k);
+    Update(1, t);
+  }
+  return rs;
+}
+
+
+}  // namespace algorithm
+
+using namespace algorithm;
+
+
+int main(int argc, char** argv) {
+  std::vector<int> v;
+  v.push_back(100);
+  v.push_back(150);
+  v.push_back(10);
+  v.push_back(300);
+  LOG(INFO) << Read(v);
+  return 0;
+}
--- algorithm/poj/2777/code.cc
+++ algorithm/poj/2777/code.cc
@@ -7,17 +7,126 @@
 
 /*
  * 线段树
- *
+ * Accepted 3700K 360MS
  * */
 #include "base/public/common_head.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <algorithm>
+
 namespace algorithm {
+
+const int MAX = 150000;  // 这是叶子节点的个数，树节点的个数至少是其两倍
+struct Node {
+  int left;
+  int right;
+  int color;
+  Node(int c = 1) : left(0), right(0), color(c){}
+};
+Node data[MAX * 3];  // note  MAX * 3
+
+int count[35] = {0};
+
+void Build(int k, int l, int r) {
+  data[k].left = l;
+  data[k].right = r;
+  if (r == l) return;
+  int mid = l + (r - l) / 2;
+  Build(2 * k, l, mid);
+  Build(2 * k + 1, mid + 1, r);
+}
+
+void Build(int n) {
+  Build(1, 1, n);
+}
+
+void Insert(int k, int l, int r, int c) {
+  // LOG(INFO) << "k:" << k << " l:" << l << " r:" << r;
+  // LOG(INFO) << data[k].left  << "~" << data[k].right;
+  if (data[k].left >= l && data[k].right <= r) {
+    data[k].color = c;
+  } else {
+    if (data[k].color > 0) {
+      data[2 * k].color = data[k].color;
+      data[2 * k + 1].color = data[k].color;
+      if (data[k].color != c) data[k].color = -1;
+    }
+    int mid = data[k].left + (data[k].right - data[k].left) / 2;
+    if (mid >= r) Insert(2 * k, l, r, c);
+    else if (mid < l) Insert(2 * k + 1, l, r, c);
+    else {
+      Insert(2 * k, l, mid, c);
+      Insert(2 * k + 1, mid + 1, r, c);
+    }
+  }
+}
+
+void Insert(int l, int r, int c) {
+  Insert(1, l, r, c);
+}
+
+void Count(int k, int l, int r) {
+  // LOG(INFO) << "k:" << k << " l:" << l << " r:" << r;
+  // LOG(INFO) << data[k].left  << "~" << data[k].right << " " << data[k].color;
+  if (data[k].color != -1) {
+    count[data[k].color]++;
+  } else {
+    int mid = data[k].left + (data[k].right - data[k].left) / 2;
+    if (mid >= r) Count(2 * k, l, r);
+    else if (mid < l) Count(2 * k + 1, l, r);
+    else {
+      Count(2 * k, l, mid);
+      Count(2 * k + 1, mid + 1, r);
+    }
+  }
+}
+
+int Count(int l, int r) {
+  memset(count, 0, sizeof(count));
+  Count(1, l, r);
+  int rs = 0;
+  for (int i = 0; i < sizeof(count) / sizeof(int); i++) {
+    if (count[i] > 0) rs++;
+  }
+  return rs;
+}
+
+void Read() {
+  freopen("test.txt", "r", stdin);
+  int L = 0;
+  int T = 0;
+  int O = 0;
+  scanf("%d%d%d", &L, &T, &O);
+  getchar();
+  Build(L);
+  for (int i = 0; i < O; i++) {
+    char ch = '\0';
+    int l = 0;
+    int r = 0;
+    int c = 0;
+    scanf("%c", &ch);
+    if (ch == 'C') {
+      scanf("%d%d%d", &l, &r, &c);
+      if (l > r) std::swap(l, r);
+      Insert(l, r, c);
+    } else {
+      scanf("%d%d", &l, &r);
+      if (l > r) std::swap(l, r);
+      printf("%d\n", Count(l, r));
+    }
+    getchar();
+  }
+}
+
 }  // namespace algorithm
 
 using namespace algorithm;
 
 
 int main(int argc, char** argv) {
-
+  Read();
   return 0;
 }
