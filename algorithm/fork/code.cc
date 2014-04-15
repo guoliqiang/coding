ed49a94a32ba2a46edf9606bab63cc0cf7ac2d79
guoliqiang <guoliqiang@ubuntu.(none)>
2014-04-15 03:13:56 -0700

    has udpated

--- algorithm/fork/code.cc
+++ algorithm/fork/code.cc
@@ -1,47 +1,68 @@
-// Copyright 2014 Liqiang Guo. All Rights Reserved.
-// Author: Liqiang Guo (guoliqiang2006@gmail.com)
-// I just want to GH to hss~
-// Date  : 2014-04-14 22:32:41
+// Copyright 2014 Jike Inc. All Rights Reserved.
+// Author: Zhongwu Zhai(zhaizhongwu@jike.com)
+// Date  : 2014-04-15 01:41:11
 // File  : code.cc
 // Brief :
 
+/*
+fork will also copy the data of heap!
+http://www.cnblogs.com/Anker/archive/2013/01/16/2862800.html
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include<sys/wait.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 #include "base/public/common_ojhead.h"
 
-namespace algorithm {
+struct Shared {
+  sem_t mutex;
+  int count;
+} shared;
 
 void Foo() {
-  int count = 0;
-  pid_t pid = fork();
-  if (pid == 0) {
-    count++;
-    LOG(INFO) << getppid() << " " << getpid() << " " << count; 
+  int nloop = 10;
+  int fd = open("mytest", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+  write(fd,&shared,sizeof(struct Shared));
+  struct  Shared *ptr = (struct  Shared *)mmap(NULL, sizeof(struct Shared), PROT_READ | PROT_WRITE,
+                                               MAP_SHARED, fd, 0);
+  close(fd);
+  if(ptr == MAP_FAILED) {
+    LOG(INFO) << "mmap error!";
   } else {
-    count++;
-    LOG(INFO) << getppid() << " " << getpid() << " " << count;
+    sem_init(&ptr->mutex,1,1);
+    setbuf(stdout, NULL);
+    pid_t pid = fork();
+    if (pid == 0) {
+      for (int i = 0; i < nloop; ++i) {
+        sem_wait(&ptr->mutex);
+        LOG(INFO) << "child:" << ptr->count++;
+        sem_post(&ptr->mutex);
+        sleep(1);
+      }
+    } else {
+      for (int i = 0;i < nloop; ++i) {
+        sem_wait(&ptr->mutex);
+        LOG(INFO) << "parent:" << ptr->count++;
+        sem_post(&ptr->mutex);
+        sleep(2);
+      }
+      int status = 0;
+      waitpid(pid, &status, 0);
+    }
   }
 }
 
-void Bar() {
-  int * count = new int(0);
-  pid_t pid = fork();
-  if (pid == 0) {
-    (*count)++;
-    LOG(INFO) << getppid() << " " << getpid() << " " << count << " " << *count; 
-  } else {
-    sleep(2);
-    LOG(INFO) << getppid() << " " << getpid() << " " << count << " " << *count;
-    (*count)+= 2;
-    LOG(INFO) << getppid() << " " << getpid() << " " << count << " " << *count;
-  }
+int main(int argc, char **argv) {
+  Foo();
 }
 
-}  // namespace algorithm
-
-using namespace algorithm;
-
-int main(int argc, char** argv) {
-  FROMFILE;
-  //Foo();
-  Bar();
-  return 0;
-}
