54cbe7aff8732b28eed73f34d8e6f491738d1782
guoliqiang <guoliqiang@ubuntu.(none)>
2014-06-04 01:21:27 -0700

    has udpated

---
+++ tools/makefile_template/internal/foo.cc
@@ -0,0 +1,13 @@
+// Copyright 2014 Jike Inc. All Rights Reserved.
+// Author: Liqiang Guo(guoliqiang2006@gmail.com)
+// Date  : 2014-06-04 00:48:37
+// File  : foo.cc
+// Brief :
+
+#include "../../base/public/logging.h"
+
+int main(int argc, char** argv) {
+  LOG(INFO) << "main";
+  return 0;
+}
+
