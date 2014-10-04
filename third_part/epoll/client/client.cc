d30dbd4f0756a4916db2b8effa5ce6f56b984975
guoliqiang <guoliqiang2006@126.com>
2014-10-04 17:50:03 +0800

    has updated

--- third_part/epoll/client/client.cc
+++ third_part/epoll/client/client.cc
@@ -5,14 +5,30 @@
 // Brief :
 #include "base/public/net.h"
 #include "base/public/logging.h"
+#include "third_part/epoll/proto/epoll_test.pb.h"
 
 int main(int argc, char** argv) {
   int fd = base::TcpConnect("127.0.0.1", 8003, 100);
-  char buffer[1000] = {0};
-  int rs = base::TcpSend(fd, buffer, 1000);
+  char buffer[100000] = {0};
+  epoll::Request request;
+  request.set_str("request guoliqiang");
+  int len = request.ByteSize();
+  char * ptr = buffer;
+  *((int *)ptr) = len + 4 + 4;
+  ptr += 4;
+  *((int *)ptr) = static_cast<int>(epoll::OPCODE);
+  ptr += 4;
+  request.SerializeToArray(ptr, len);
+  int rs = base::TcpSend(fd, buffer, len + 8);
   LOG(INFO) << rs;
-  int len = base::TcpRecv(fd, buffer, 1000, 100);
-  LOG(INFO) << len;
-  LOG(INFO) << buffer;
+  len = base::TcpRecv(fd, buffer, 100000, 100);
+  epoll::Response response;
+  ptr = buffer;
+  len = *((int *) ptr) - 4;
+  ptr += 4;
+  response.ParseFromArray(ptr, len);
+  LOG(INFO) << response.DebugString();
+
+  close(fd);
   return 0;
 }
--- third_part/epoll/server/server.cc
+++ third_part/epoll/server/server.cc
@@ -1,13 +1,12 @@
 // Copyright 2014 Jike Inc. All Rights Reserved.
-// Date  : 2014-05-06 20:20:33
-// File  : server.cc
-// Brief :
+// Date  : 2014-05-06 20:20:33 // File  : server.cc // Brief :
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include "base/public/thread.h"
 #include "../public/ependingpool.h"
 #include "base/public/net.h"
+#include "third_part/epoll/proto/epoll_test.pb.h"
 
 class LisenThread : public base::Thread {
  public:
@@ -40,14 +39,64 @@ class WorkThread : public base::Thread {
     int handle;
     int fd;
     std::string str = "hello word!";
+    char read_buffer[100000] = {'\0'};
     while (pool_->is_run()) {
       if(pool_->fetch_item(&handle, &fd) != 0) {
         LOG(INFO) << "continue";
         continue;
       }
-      LOG(INFO) << str;
-      base::TcpSend(fd, str.c_str(), str.size());
+      int len = base::TcpRecv(fd, read_buffer, sizeof(read_buffer), 100);
+      char * ptr = read_buffer;
+      len = *((int *)ptr);
+      ptr += 4;
+      int cmd = *((int *)ptr);
+      ptr += 4;
+      len -= 8;
+      epoll::Request request;
+      request.ParseFromArray(ptr, len);
+      LOG(INFO) << cmd << " " << request.DebugString();
+
+      epoll::Response response;
+      response.set_str("response guoliqiang");
+      ptr = read_buffer;
+      len = response.ByteSize() + 4;
+      *((int *) ptr) = len;
+      ptr += 4;
+      response.SerializeToArray(ptr, response.ByteSize());
+      base::TcpSend(fd, read_buffer, len);
+
       pool_->reset_item(handle, false);
+    }
+  }
+ private:
+  ependingpool * pool_;
+};
+
+// long conn
+class WorkThreadLongConn : public base::Thread {
+ public:
+  WorkThreadLongConn(ependingpool * pool) : base::Thread(true) {
+    pool_ = pool;
+  }
+ protected:
+  virtual void Run() {
+    int handle;
+    int fd;
+    std::string str = "hello word!";
+    char read_buffer[100000] = {'\0'};
+    while (pool_->is_run()) {
+      if(pool_->fetch_item(&handle, &fd) != 0) {
+        continue;
+      }
+      if (base::TcpRecv(fd, read_buffer, sizeof(read_buffer), 100) < 0) {
+        pool_->reset_item(handle, false); // close the handle
+        LOG(INFO) << "read error!";
+        continue;
+      }
+      LOG(INFO) << "read:" << read_buffer;
+      if (base::TcpSend(fd, str.c_str(), str.size()) == -1) {
+        pool_->reset_item(handle, false); // close the handle
+      } else pool_->reset_item(handle, true);  // keep the handle
     }
   }
  private:
@@ -59,6 +108,7 @@ int main(int argc, char** argv) {
   pool.set_conn_timeo(10);
   LisenThread lisen(&pool);
   WorkThread work(&pool);
+  // WorkThreadLongConn work(&pool);
   lisen.Start();
   work.Start();
   lisen.Join();
