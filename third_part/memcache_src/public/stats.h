6d69ee1bd8ce1ba953c600d5152cc6662de51aac
guoliqiang <guoliqiang@ubuntu.(none)>
2014-12-17 05:16:48 -0800

    has updated

---
+++ third_part/memcache_src/public/stats.h
@@ -0,0 +1,7 @@
+void stats_prefix_init(void);
+void stats_prefix_clear(void);
+void stats_prefix_record_get(const char *key, const size_t nkey,
+                             const bool is_hit);
+void stats_prefix_record_delete(const char *key, const size_t nkey);
+void stats_prefix_record_set(const char *key, const size_t nkey);
+char *stats_prefix_dump(int *length);
